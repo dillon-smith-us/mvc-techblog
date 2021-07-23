c8a5305828c9784d4ec75f710c6b4b4dbac668ec
Dillon Smith <dillsmth@gmail.com>
2021-07-22 21:50:53 -0500

    userdata gets

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,22 +18,27 @@ router.get('/', async (req, res) => {
 // get one user
 router.get('/:id', async (req, res) => {
   try {
-    
+    const dbUserData = await User.findOne({
+      attributes: {
+        exclude: ['password']
+      },
+    });
+    res.status(200).json(dbUserData);
   }
 })
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const dbUserData = await User.findOne({ where: { email: req.body.email } });
 
-    if (!userData) {
+    if (!dbUserData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
@@ -43,10 +48,10 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = dbUserData.id;
       req.session.logged_in = true;
       
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
